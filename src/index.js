9a4704dc34625c659ab9292d8bdf9a2eba54082b
Rebecca <graham.cali@gmail.com>
2021-03-03 17:53:33 -0600

    implemented oo-js refactor

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
    getSyllabi()
     // fetch and load Syllabi 
    
-
    const createSyllabusForm = document.querySelector("#create-syllabus-form")
 
    createSyllabusForm.addEventListener("submit", (e) => createFormHandler(e))
-   
 })
 
 function getSyllabi() {
@@ -17,22 +15,13 @@ function getSyllabi() {
     .then(syllabi => {
         syllabi.data.forEach(syllabus => {
             // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-            render(syllabus)
+            
+            let newSyllabus = new Syllabus(syllabus, syllabus.attributes)
+            
+            document.querySelector('#syllabus-container').innerHTML += newSyllabus.renderSyllabusCard()
+            // render(syllabus)
           })
     })
-}
-
-function render(syllabus) {
-    const syllabusMarkup = `
-              <div data-id=${syllabus.id}>
-                <img src=${syllabus.attributes.image_url} height="200" width="250">
-                <h3>${syllabus.attributes.title}</h3>
-                <p>${syllabus.attributes.category.name}</p>
-                <button data-id=${syllabus.id}>edit</button>
-              </div>
-              <br><br>`;
-    
-              document.querySelector('#syllabus-container').innerHTML += syllabusMarkup
 }
 
 function createFormHandler(e) {
